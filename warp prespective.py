c5faf4380547fa2a6a59d29b96bc4011f3f3501c
RACHAKONDA SIVA SAI ABHISHEK <99978100+RachakondaSivaSaiAbhishek@users.noreply.github.com>
2023-06-08 22:06:11 +0530

    Create warp prespective.py
    warping image with respective to dimentions

---
+++ warp prespective.py
@@ -0,0 +1,20 @@
+import cv2 
+import numpy as np
+
+img = cv2.imread("Resources/cards.jpg")
+
+width,height = 250,350
+
+pts1 = np.float32([[111,219]1[287,188],[154,428],[352,440]])
+
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+
+matrix = cv2.getPOrespectiveTransform(pts1,pts2)
+
+imgOutput = cv2,warpPerspective(img,matrix,(width,height))
+
+cv2.imshow("Image",img)
+
+cv2.imshow("Output image",imOutput)
+
+cv2.waitKey(0)
